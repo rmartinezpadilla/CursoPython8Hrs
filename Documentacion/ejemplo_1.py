bdf8534ea075136bd515bef73954da99818a4b91
rmartinezpadilla <rmartinezpadilla@hotmail.com>
2023-10-31 15:55:28 -0500

    Expresiones regulares

---
+++ Documentacion/ejemplo_1.py
@@ -0,0 +1,13 @@
+def suma(numa, numb):
+    
+    """ Esta función realiza la suma de 
+        do números cualera, ya sean
+        enteros o reales"""
+        
+    return numa+numb
+
+print(suma(20, 3.4))
+
+#aqui imprimimos la documentacion del método
+
+print(suma.__doc__)
