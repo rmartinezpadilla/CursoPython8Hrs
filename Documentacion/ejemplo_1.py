102fdf1f4b862f54e4f9656bd04c831b99ca3068
rmartinezpadilla <rmartinezpadilla@hotmail.com>
2023-10-31 16:03:05 -0500

    Documentacion

--- Documentacion/ejemplo_1.py
+++ Documentacion/ejemplo_1.py
@@ -1,13 +1,23 @@
-def suma(numa, numb):
-    
-    """ Esta función realiza la suma de 
-        do números cualera, ya sean
-        enteros o reales"""
+class metodos:
+    """Comentartios de clase
+        aqui se crearon metodos para practicar 
+        la documentacion de metodos y clases"""
         
-    return numa+numb
+    def suma(numa, numb):
+        
+        """ Esta función realiza la suma de 
+            do números cualera, ya sean
+            enteros o reales"""
+            
+        return numa+numb
 
-print(suma(20, 3.4))
+    print(suma(20, 3.4))
 
-#aqui imprimimos la documentacion del método
+    #aqui imprimimos la documentacion del método
 
-print(suma.__doc__)
+    print(suma.__doc__)
+
+#usando help
+help(metodos.suma)
+
+help(metodos)
